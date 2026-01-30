739ce05b5c4446e9183cf20f7c68696870c6e4a1
AnotherOne07 <natsujunior7@gmail.com>
2026-01-30 18:34:37 -0300

    doing hirschberg, linear_space_alignment almost finish

--- src/alignment/hirschberg.cpp
+++ src/alignment/hirschberg.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 #include <string>
 #include "utils/alignment.cpp"
+#include "src/alignment/needleman.cpp"
 
 // namespace serve como uma folder para as variáveis
 // declarar um namespace global ajuda a legibilidade do código
 // porém em projetos maiores deve ser evitado para não causar erros de escopo
 using namespace std;
+
+
 
 int main (){
     string X = "ACCGA";
@@ -15,17 +18,27 @@ int main (){
     int xAxis, yAxis;
     xAxis = X.length() + 1;
     yAxis = Y.length() + 1;
-    vector<vector<int>> m(xAxis, vector<int>(yAxis, 0));
+    vector<vector<int>> m(xAxis, vector<int>(2, 0));
+
+    // Meio da matriz
+    int k = xAxis/2;
 
     // Preenchendo coluna base
     for(int i = 0; i < xAxis; i++){
         m[i][0] = i * gap;
     }
+   
+    m[0][1] = j * gap;
+    Y1 = Y.substr(0,k+1);
+    Y2 = Y.substr(k+1);
+    space_efficient_alignment(X,Y1,gap,mismatch,match,m,j);
+    backward_space_efficient_alignment(X,Y2,gap,mismatch,m,j);
+	// q representa o índice que minimiza f(q,n/2) + g(q,n/2)
+	//q = 
+	// adiciona (q,n/2) à lista global P
+	// divide_and_conquer_alignment()
+	// divide_and_conquer_alignment()
     
-    for (int j = 1; j < yAxis; j++){
-        m[0][1] = j * gap;
-        space_efficient_alignment();
-    }
 
 
     return 0;
