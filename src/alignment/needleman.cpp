37d4c271efc76f2774d18518998fad9a1efc673a
Francisco Junior <natsujunior7@gmail.com>
2026-01-30 08:42:38 -0300

    needleman is kind of working

--- src/alignment/needleman.cpp
+++ src/alignment/needleman.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -28,6 +29,40 @@ int compare_directions(int top, int left, int diagonal, int gap, int mismatch, i
 	return result;
 }
 
+void alignment(string X, string Y, int xAxis, int yAxis, vector<vector<int>> &m){
+    string newX = "", newY = "";
+    int top, left;
+    int i, j;
+    i = xAxis;
+    j = yAxis;
+    while(true){
+        if (i == 0 || j == 0)
+            break;
+
+        top = m[i-1][j];
+        left = m[i][j-1];
+
+        if (X[i-1] == Y[j-1]){
+            newX = X[i-1] + newX;
+            newY = Y[j-1] + newY;
+            i = i - 1;
+            j = j - 1;
+        }
+        else if (top < left){
+            newX = X[i-1] + newX;
+            newY = "-" + newY; 
+            i = i - 1;
+        }
+        else {
+            newX = "-" + newX;
+            newY = Y[j-1] + newY;
+            j = j - 1;
+        }
+    }
+    cout << newX << "\n";
+    cout << newY << "\n";
+    return;
+}
 /*
     Gaps: onde inserimos ou removemos caracteres
     Incompatibilidades (mismatches): onde os caracteres são diferentes
@@ -51,12 +86,13 @@ int main(){
     xAxis = X.length() + 1;
     yAxis = Y.length() + 1;
 
-    int m[xAxis][yAxis];
+    // int m[xAxis][yAxis];
+    vector<vector<int>> m (xAxis, vector<int>(yAxis,0));
 
     int gap=2, mismatch=1, match=0;
 
-    cout << X +  ": " << xAxis << "\n";
-    cout << Y +  ": " << yAxis << "\n";
+    // cout << X +  ": " << xAxis << "\n";
+    // cout << Y +  ": " << yAxis << "\n";
 
     // Preenchendo a linha e coluna 0, que representam os gaps
     m[0][0] = 0;
@@ -76,13 +112,13 @@ int main(){
     */
     for(int i=1;i<xAxis;i++){
         for(int j=1;j<yAxis;j++){
-            	// m[i][j] = 0;
-		bool is_match; 
-		is_match = X[i] == Y[j];
-		cout << "\nIndíce: [" << i << "]["<< j<< "]: " ;
-    		int result = compare_directions(m[i-1][j], m[i][j-1], m[i-1][j-1], gap, mismatch, match,is_match);
-		m[i][j] = result;		
-        }
+            // m[i][j] = 0;
+            bool is_match; 
+            is_match = X[i] == Y[j];
+            // cout << "\nIndíce: [" << i << "]["<< j<< "]: " ;
+            int result = compare_directions(m[i-1][j], m[i][j-1], m[i-1][j-1], gap, mismatch, match,is_match);
+            m[i][j] = result;		
+            }
     }
 
     for(int i=0;i<yAxis;i++){
@@ -91,6 +127,8 @@ int main(){
         }
         cout << "\n";
     }
+
+    alignment( X, Y, xAxis, yAxis, m);
 
     return 0;
 }
